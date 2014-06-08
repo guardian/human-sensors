f7609509823dde0f52c8b52994edc9c7895d4e3f
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-06-08 16:15:43 +0100

    Return results when recording answer, better CORS

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -10,7 +10,7 @@ object Api extends Controller {
 
   var answers = List[Answer]()
 
-  val corsHeaders = ("Access-Control-Allow-Origin", "*")
+  val corsHeaders = Seq(("Access-Control-Allow-Origin" -> "*"),("Access-Control-Allow-Headers", "Content-Type"))
 
   def topics(session: String) = Action {
     val next = for {
@@ -21,16 +21,34 @@ object Api extends Controller {
     next.headOption match {
       case Some((topic: Topic, question: Question)) =>
         Ok(Json.obj("topic" -> topic, "question" -> question, "replyUrl" -> s"/api/topics/${topic.id}/${question.id}")).
-          withHeaders(corsHeaders)
+          withHeaders(corsHeaders:_*)
       case _ =>
         NotFound
     }
   }
 
+  // FIXME: lost every time?
+  def listAnswers(topicId: String, questionId: String) = Action {
+    val matchingAnswers = answers.filter(answer => answer.topic == topicId && answer.question == questionId)
+    Ok(Json.arr(matchingAnswers)).withHeaders(corsHeaders:_*)
+  }
+
   def recordAnswer(topicId: String, questionId: String) = Action(parse.json) { request =>
     val newAnswer = request.body.as[Answer]
     answers ::= newAnswer
-    Ok("").withHeaders(corsHeaders)
+
+    val topicAnswers = answers.filter(_.topic == topicId)
+    val results = topicAnswers.
+      groupBy(_.value).
+      mapValues((_.size.toDouble / topicAnswers.size * 100)).
+      toList
+
+    Ok(Json.obj("results" -> results.map(res => Json.obj("answer" -> res._1, "percent" -> res._2)))).
+      withHeaders(corsHeaders:_*)
+  }
+
+  def corsOk(topicId: String, questionId: String) = Action {
+    Ok.withHeaders(corsHeaders:_*)
   }
 
 }
